55b0e18ec93f4b17bc1377e01ead10f786c52037
Patryk Kulpa <patkul004@pbs.edu.pl>
2024-11-30 22:42:31 +0100

    added main page

---
+++ src/components/mainPage/MainPage.jsx
@@ -0,0 +1,70 @@
+import Navbar from "../navbar/Navbar";
+import styles from "./MainPage.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+const MainPage = () => {
+  const data = [
+    {
+      paragraph:
+        " Bezpłatna rehabilitacja dla mieszkańców kujawsko-pomorskiego",
+      date: "30.0.2024",
+    },
+    {
+      paragraph:
+        " Bezpłatna rehabilitacja dla mieszkańców kujawsko-pomorskiego",
+      date: "30.0.2024",
+    },
+    {
+      paragraph:
+        " Bezpłatna rehabilitacja dla mieszkańców kujawsko-pomorskiego",
+      date: "30.0.2024",
+    },
+  ];
+
+  return (
+    <section className={styles.container}>
+      <Navbar />
+      <main className={styles.main}>
+        <h2 className={styles.heading}>Aktualności</h2>
+        <section className={styles.header}>
+          {data.map((e, idx) => (
+            <div className={styles.card} key={idx}>
+              <p className={styles.card_date}>{e.date}</p>
+              <div className={styles.card_desc}>
+                <p>{e.paragraph}</p>
+              </div>
+            </div>
+          ))}
+        </section>
+        <section className={styles.header2}>
+          {data.map((e, idx) => (
+            <div className={styles.card} key={idx}>
+              <p className={styles.card_date}>{e.date}</p>
+              <div className={styles.card_desc}>
+                <p>{e.paragraph}</p>
+              </div>
+            </div>
+          ))}
+        </section>
+        <div className={styles.list_num}>
+          <FontAwesomeIcon icon={faArrowLeft} className={styles.arrow_icn} />
+          <span className={styles.active}>1</span>
+          <span>2</span>
+          <span>3</span>
+          <FontAwesomeIcon icon={faArrowRight} className={styles.arrow_icn} />
+        </div>
+
+        <section className={styles.accessibility_cont}>
+          <p>Dostępność</p>
+          <div className={styles.accessibility_blocks}>
+            <div>Dokumenty do pobrania</div>
+            <div>Link</div>
+          </div>
+        </section>
+      </main>
+      <footer className={styles.footer}></footer>
+    </section>
+  );
+};
+
+export default MainPage;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,66 @@
+import styles from "./Navbar.module.css";
+import logo from "./../../assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+const Navbar = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.logo}>
+        <img src={logo} alt="logo hotel homer" />
+      </div>
+      {/* <section className={styles.accessibility_btns}>
+        <div className={styles.font_btns}>
+          <button>A-</button>
+          <button>A</button>
+          <button>A+</button>
+        </div>
+        <div className={styles.color_btns}>
+          <button aria-label="Czarno-biały"></button>
+          <button aria-label="Zwykły"></button>
+          <button aria-label="Pomarańczowo-czarny"></button>
+          <button aria-label="Czarno-pomarańczowy"></button>
+        </div>
+      </section> */}
+      <section className={styles.menu}>
+        <button onClick={() => setIsClicked(!isClicked)}>
+          <FontAwesomeIcon icon={faBars} className={styles.bars_icn} />
+        </button>
+      </section>
+
+      {isClicked && (
+        <section className={styles.hidden_menu}>
+          <form className={styles.menu_form}>
+            <span className={styles.search_input}>
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                className={styles.search_icn}
+              />
+              <input type="text" placeholder="Wyszukaj frazę" />
+            </span>
+            <ul className={styles.menu_list}>
+              <li>
+                <a href="">Akutalności</a>
+              </li>
+              <li>
+                <a href="">O nas</a>
+              </li>
+              <li>
+                <a href="">Oferty</a>
+              </li>
+              <li>
+                <a href="">Dojazd</a>
+              </li>
+              <li>
+                <a href="">Kontakt</a>
+              </li>
+            </ul>
+          </form>
+        </section>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
